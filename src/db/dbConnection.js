36b923d1e09451c93dc2b3a883f77d74005a5f9e
Bharat yadav <bharat.yadav@zehntech.com>
2025-03-17 19:11:43 +0530

    successfully connected the mongodb data base

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../contants.js";
 
 const  dbConnection = async () => {
-    console.log("dbconnection is called:",process.env.MONGODB_URI,DB_NAME);
+    //console.log("dbconnection is called:",process.env.MONGODB_URI,DB_NAME);
     
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${ DB_NAME}`)
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import 'dotenv/config'
-import express from "express"
 import dbConnection from './db/dbConnection.js';
-const app = express();
+import { app } from './app.js';
+// const app = express();
 
-dbConnection();
+dbConnection()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running on port ${process.env.PORT}`)
+    })
+    app.on("error",(err)=>{
+        console.log("error", err)
+    })
+})
+.catch((err)=>{
+    console.log("mongo db connection failed",err)
+})
 
-app.listen(3000,()=>{
-    console.log("server is running on port 3000")
+
+app.get("/",(req,res)=>{
+    res.send("<h1>hello world</h1>")
 })
