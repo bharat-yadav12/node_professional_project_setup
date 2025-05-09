8591bc5848002a0b930897587ac1c932d2835158
Bharat yadav <bharat.yadav@zehntech.com>
2025-05-09 19:28:02 +0530

    add getWatchHistory controller and corresponding route; update user routes for account details and avatars

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import express, { Router } from 'express';
-import { createUser,getAllUsers,getUserById,updateUser,deleteUser,registerUser,loginUser,logoutUser,tempController ,refreshAccessToken,changeCurrentPassword,getCurrentUser} from '../controllers/user.controller.js';
+import { createUser,getAllUsers,getUserById,updateUser,deleteUser,registerUser,loginUser,logoutUser,tempController ,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile,getWatchHistory} from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
 import { addTempvar } from '../middlewares/temp.middleware.js';
 import {verifyJWT} from '../middlewares/auth.middleware.js';
@@ -29,6 +29,12 @@ router.route('/logout').post(verifyJWT,logoutUser);
 router.route('/refresh-token').post(refreshAccessToken);
 router.route('/change-password').post(verifyJWT,changeCurrentPassword);
 router.route('/current-user').get(verifyJWT,getCurrentUser);
+router.route('/update-account').patch(verifyJWT, updateAccountDetails);
+router.route('/update-avatar').patch(verifyJWT,upload.single('avatar'),updateUserAvatar);
+router.route('/update-cover-image').patch(verifyJWT,upload.single('coverImage'),updateUserCoverImage);
+router.route('/c/:username').get(verifyJWT, getUserChannelProfile);
+router.route('/history').get(verifyJWT,getWatchHistory);
+
 // an example of using a custome middleware to add a temporary variable to the request object
 router.route('/temp').get(addTempvar,tempController)
 
